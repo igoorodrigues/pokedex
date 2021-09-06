e3f07ba9d2fb851dbe8d0ace9ec7900664fb0ac1
igoorodrigues <igoor.rodriguess@outlook.com>
2021-09-06 20:04:06 -0300

    Created the pages module and the home component

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Pages
+import { HomeComponent } from './home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
