b5952950757f3919d403bc49c76dcc02718d6e6b
igoorodrigues <igoor.rodriguess@outlook.com>
2021-09-06 23:07:35 -0300

    Creating search bar and starting poke list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,14 +5,20 @@ import { RouterModule } from '@angular/router';
 
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports: [
     PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule,
